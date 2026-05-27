32874488b4c8aefc2f86c40bd6b9e389cee70c2f
dimkoug <dimkoug@gmail.com>
2026-05-27 22:35:13 +0300

    feat: add celery app and fetch_rates task

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -1,8 +1,24 @@
 import os
 from celery import Celery
+from celery.schedules import schedule
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery("currency")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    from django.conf import settings
+    sender.add_periodic_task(
+        schedule(run_every=settings.POLL_INTERVAL_SECONDS),
+        fetch_rates_signature(),
+        name="poll-forex-rates",
+    )
+
+
+def fetch_rates_signature():
+    from rates.tasks import fetch_rates
+    return fetch_rates.s()
